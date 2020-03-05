679bfb9b40afe424d5d650c73fb30a50585d6ff6
Natalia Sinitsyna (intern) <sokolova.natawa@gmail.com>
2020-03-05 14:14:35 +0300

    Initial commit

---
+++ TheMovieDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Natali on 05.03.2020.
+//  Copyright © 2020 Redmadrobot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
