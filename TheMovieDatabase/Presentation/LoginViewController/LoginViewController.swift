e04923c1b58218eb3b4deb25c46257e44a569dc7
Natalia Sinitsyna (intern) <kosyak_@mail.ru>
2020-03-06 18:41:43 +0300

    Добавлен SwiftLint.

---
+++ TheMovieDatabase/Presentation/LoginViewController/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Natali on 06.03.2020.
+//  Copyright © 2020 Redmadrobot. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
