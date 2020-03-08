d3b37fc7382a3a23175da1f3830c04697f34c1fd
Natalia Sinitsyna (intern) <kosyak_@mail.ru>
2020-03-09 00:01:34 +0300

    Добалены view controllers для основных экранов, расширения для классов, цвета.

---
+++ Common/Extensions/TextFieldExtension.swift
@@ -0,0 +1,9 @@
+//
+//  TextFieldExtension.swift
+//  TheMovieDatabase
+//
+//  Created by Natali on 07.03.2020.
+//  Copyright © 2020 Redmadrobot. All rights reserved.
+//
+
+import Foundation
---
+++ Common/Extensions/ViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerExtension.swift
+//  TheMovieDatabase
+//
+//  Created by Natali on 07.03.2020.
+//  Copyright © 2020 Redmadrobot. All rights reserved.
+//
+
+import Foundation
